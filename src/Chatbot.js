b10a8fb7b62bdd26aa8b170aaac0cd426e60e180
Vadia Sabira (ID) <vadia.sabira@homecredit.co.id>
2019-05-29 11:11:52 +0700

    chatbot

--- src/Chatbot.js
+++ src/Chatbot.js
@@ -1,242 +1,126 @@
-
-
-
-
-
-import React from"react";
-
-import{useState}from"react";
-
+import React from "react";
+import { useState } from "react";
 import "./Chatbot.css";
-
 import axios from "axios";
- 
 
-const Suggestion=props=>{
-
-return (
-
-<div className="suggestionContainer">
-
-<div className="suggestMsg">
-
- Here are some of the most frequently asked questions:
-
-</div>
-
-<div className="suggestButtonContainer">
-
-<div onClick={props.click} className="suggestButton">
-
- Apa yang saya perlukan untuk mengajukan pembiayaan?
-
-</div>
-
-<div onClick={props.click} className="suggestButton">
-
- Apa itu DONA?
-
-</div>
-
-<div onClick={props.click} className="suggestButton">
-
- Apa itu FlexiFast?
-
-</div>
-
-</div>
-
-</div>
-
- );
-
-};
- 
-
-const Chatbot=()=>{
-
-const[question,setQuestion]=useState([]);
-
-// const [answer, setAnswer] = useState([]);
-
-const[inputQuestion,setInputQuestion]=useState("");
-
-// const [isAsking, setIsAsking] = useState([]);
-
-const[suggestion,setSuggestion]=useState([]);
- 
-
-const handleSuggestion=()=>{
-
-console.log("clicked");
-
-return setSuggestion===<Answer answer="jjajaja"/>;
-
-};
- 
-
-const handleClick=()=>{
-
-const message={
-
- msg:inputQuestion
-
+const Footer = props => {
+  const { change, keyUp, keyDown, value, clickHandler } = props;
+  return (
+    <div className="chatFooter">
+      <div className="chatTextField">
+        <input
+          onChange={change}
+          onKeyUp={keyUp}
+          onKeyDown={keyDown}
+          value={value}
+        />
+        <button onClick={clickHandler}>send</button>
+      </div>
+    </div>
+  );
 };
 
-axios
+const Suggestion = props => {
+  const { click } = props;
+  return (
+    <div className="suggestionContainer">
+      <div className="suggestMsg">
+        Here are some of the most frequently asked questions:
+      </div>
 
-.post("https://jsonplaceholder.typicode.com/users",message)
+      <div className="suggestButtonContainer">
+        <div onClick={click} className="suggestButton">
+          Apa yang saya perlukan untuk mengajukan pembiayaan?
+        </div>
 
-.then(res=>{
+        <div onClick={click} className="suggestButton">
+          Apa itu DONA?
+        </div>
 
-console.log(res.data);
-
-});
-
-setQuestion(prevState=> [...prevState,message]);
-
-setInputQuestion("");
-
-};
- 
-
-const handleKeyDown=e=>{
-
-if (e.keyCode===13) {
-
-const message={
-
- msg:inputQuestion
-
+        <div onClick={click} className="suggestButton">
+          Apa itu FlexiFast?
+        </div>
+      </div>
+    </div>
+  );
 };
 
-axios
+const Chatbot = () => {
+  const [inputQuestion, setInputQuestion] = useState("");
+  // const [suggestion, setSuggestion] = useState("");
+  const [answer, setAnswer] = useState(answer);
+  const [chatArray, setChatArray] = useState([]);
+  // const [data, setData] = useState([]);
+  // const handleSuggestion = () => {
+  //   console.log("clicked");
+  //   return setSuggestion(<Answer answer="hiayyy" />);
+  // };
 
-.post("https://jsonplaceholder.typicode.com/users",message)
+  const handleClick = () => {
+    axios
+      .get(`http://10.58.92.205:8989/question/split/${inputQuestion}`)
+      .then(res => {
+        setAnswer(res.data.map(data => data.answer));
+        const chats = {
+          qu: inputQuestion,
+          an: answer
+        };
+        console.log(chats);
+        setChatArray(prevState => [...prevState, chats]);
+        
+      });
 
-.then(res=>{
+    setInputQuestion("");
+  };
 
-console.log(res.data);
+  const onType = event => {
+    setInputQuestion(event.target.value);
+  };
 
-});
+  const Answer = props => {
+    return (
+      <div className="chatBubbleContainerAnswer">
+        <div className="chatBubbleAnswer">{props.answerMessage}</div>
+      </div>
+    );
+  };
+  const Question = props => {
+    return (
+      <div className="chatBubbleContainerQuestion">
+        <div className="chatBubbleQuestion">{props.questionMessage}</div>
+      </div>
+    );
+  };
 
-setQuestion(prevState=> [...prevState,message]);
+  return (
+    <div className="chatbotContainer">
+      <div className="chatHeader">
+        <span>Chat</span>
+      </div>
 
-setInputQuestion("");
+      <div className="chatBody">
+        <Answer answerMessage="welcome message here" />
 
-}
+        {chatArray.map((chat, index) => {
+          return (
+            <div key={`question-${index}`}>
+              <Question questionMessage={chat.qu} />
+              <Answer answerMessage={chat.an} />
+            </div>
+          );
+        })}
+      </div>
 
+      <Footer
+        change={onType}
+        keyUp={onType}
+        keyDown={event =>
+          event.keyCode === 13 ? handleClick() : event.preventDefault
+        }
+        value={inputQuestion}
+        clickHandler={handleClick}
+      />
+    </div>
+  );
 };
- 
-
-const onType=event=>{
-
-setInputQuestion(event.target.value);
-
-};
- 
-
-const Answer=props=>{
-
-return (
-
-<div className="chatBubbleContainerAnswer">
-
-<div className="chatBubbleAnswer">{props.answer}</div>
-
-</div>
-
- );
-
-};
- 
-
-const Question=props=>{
-
-return (
-
-<div className="chatBubbleContainerQuestion">
-
-<div className="chatBubbleQuestion">{props.question}</div>
-
-</div>
-
- );
-
-};
- 
-
-console.log(question);
-
-// console.log(answer);
-
-return (
-
-<div className="chatbotContainer">
-
-<div className="chatHeader">
-
-<span>Chat</span>
-
-</div>
-
-<div className="chatBody">
-
-<Answer answer="welcome message here"/>
-
-<Answer answer={<Suggestion click={()=>handleSuggestion()}/>}/>
-
-{suggestion}
-
-{question.map(el=>{
-
-return (
-
-<div>
-
-<Question question={el.msg}/>
-
-<Answer answer={el.msg}/>
-
-</div>
-
- );
-
-})}
-
-</div>
-
-<div className="chatFooter">
-
-<div className="chatTextField">
-
-<input
-
-onChange={onType}
-
-onKeyUp={onType}
-
-onKeyDown={handleKeyDown}
-
-value={inputQuestion}
-
-/>
-
-<button onClick={handleClick}>send</button>
-
-</div>
-
-</div>
-
-</div>
-
- );
-
-};
- 
-
 export default Chatbot;
-
-
-
-
